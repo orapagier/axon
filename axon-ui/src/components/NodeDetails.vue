85c2cced849ed971a0e0f3c0caddcc1a1fd126ca
Jelmar Orapa <orapajelmar@gmail.com>
2026-06-26 06:42:28 +0800

    claude: auto-backup 06:42:27

--- axon-ui/src/components/NodeDetails.vue
+++ axon-ui/src/components/NodeDetails.vue
@@ -1488,7 +1488,7 @@ onUnmounted(() => {
                 <div v-if="shouldShowProperty(prop)" class="form-row" :class="'row-' + prop.type">
                   <div v-if="prop.type === 'curlImport'" class="curl-import-row">
                     <button class="btn-curl-import" @click="openCurlImport">
-                      <span class="btn-icon">📋</span>
+                      <svg class="btn-icon" viewBox="3 0 18 22" width="13" height="16" aria-hidden="true"><path fill="currentColor" d="M19 2h-4.18C14.4.84 13.3 0 12 0c-1.3 0-2.4.84-2.82 2H5c-1.1 0-2 .9-2 2v16c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V4c0-1.1-.9-2-2-2zm-7 0c.55 0 1 .45 1 1s-.45 1-1 1-1-.45-1-1 .45-1 1-1zm7 18H5V4h2v3h10V4h2v16z"/></svg>
                       Import from cURL
                     </button>
                   </div>
